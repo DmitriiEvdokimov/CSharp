7355c00435d8823c688eb79f440ac11999faa97e
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2022-09-28 23:31:36 +0300

    SumMod

---
+++ Ex003_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{   
+        static void Main(string[] args)
+    {
+        Console.WriteLine ("Сумма двух чисел");
+        Console.WriteLine ("Введите первое число:");
+        string S1 = Console.ReadLine();
+        int x = int.Parse(S1);
+        Console.WriteLine ("Введите второе число:");
+        string S2 = Console.ReadLine();
+        int y = int.Parse(S2);
+        int result = x + y;
+        Console.Write ("Результат:");
+        Console.WriteLine (result);
+    }
+}
